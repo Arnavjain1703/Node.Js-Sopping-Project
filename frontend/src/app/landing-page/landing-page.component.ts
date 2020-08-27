fb49d3bddd16ab2c26dbc9bbadf0c2f8ca175e12
Arnavjain1703 <arnavjainkht@gmail.com>
2020-08-28 04:07:54 +0530

    Added carosule

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
-
+  @ViewChild('click',{static:false}) click: ElementRef<HTMLElement>;
+  @ViewChild('click2',{static:false}) click2: ElementRef<HTMLElement>;
+  @ViewChild('click3',{static:false}) click3: ElementRef<HTMLElement>;
+  @ViewChild('click4',{static:false}) click4: ElementRef<HTMLElement>;
   constructor() { }
-
+   images:any[]=["../../assets/photoshop.png","../../assets/photoshop2.png"]
+   interval:any;
   ngOnInit(): void {
+    this.start()
   }
+  start()
+ {
+     this.interval = setInterval(()=>
+     {
+      let el: HTMLElement = this.click.nativeElement;
+      let el2: HTMLElement = this.click2.nativeElement;
+      let el3: HTMLElement = this.click3.nativeElement;
+      let el4: HTMLElement = this.click4.nativeElement;
+          el.click()
+          el2.click()
+          el3.click()
+          el4.click()
+
+
+     },3000)
+ }
+ 
+stop()
+{
+     clearInterval(this.interval)
+}
 
 }
